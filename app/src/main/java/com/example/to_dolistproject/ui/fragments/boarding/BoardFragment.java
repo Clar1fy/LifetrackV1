8b39af81b08f140f9a75c2b2120338dc049ca76a
Clar1fy <iamonmydoctorpath@gmail.com>
2022-01-20 18:04:54 +0600

    add create task dialog including date & frequency choice

--- app/src/main/java/com/example/to_dolistproject/ui/fragments/boarding/BoardFragment.java
+++ app/src/main/java/com/example/to_dolistproject/ui/fragments/boarding/BoardFragment.java
@@ -2,6 +2,7 @@ package com.example.to_dolistproject.ui.fragments.boarding;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,15 +11,17 @@ import android.view.ViewGroup;
 
 import com.example.to_dolistproject.databinding.FragmentBoardBinding;
 
+import java.util.Calendar;
+
 
 public class BoardFragment extends Fragment {
     private FragmentBoardBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentBoardBinding.inflate(inflater, container, false);
 
         return binding.getRoot();
-    }
+            }
 }
