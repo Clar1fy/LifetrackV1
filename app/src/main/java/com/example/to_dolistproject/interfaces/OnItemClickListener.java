7428cb82d6ef4d95d391ad908c2edeba55691866
Clar1fy <iamonmydoctorpath@gmail.com>
2022-01-24 19:01:52 +0600

    adjusted adapter,recycler and AlertDialog

--- app/src/main/java/com/example/to_dolistproject/interfaces/OnItemClickListener.java
+++ app/src/main/java/com/example/to_dolistproject/interfaces/OnItemClickListener.java
@@ -3,7 +3,5 @@ package com.example.to_dolistproject.interfaces;
 import com.example.to_dolistproject.data.NoteModel;
 
 public interface OnItemClickListener {
-    void onItemClick(NoteModel noteModel);
-
-    void onItemPress(NoteModel noteModel);
+    void onItemPress(NoteModel model);
 }
--- app/src/main/java/com/example/to_dolistproject/ui/fragments/bottom_navigation/HomeFragment.java
+++ app/src/main/java/com/example/to_dolistproject/ui/fragments/bottom_navigation/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.to_dolistproject.ui.fragments.bottom_navigation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,14 +14,16 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.to_dolistproject.App;
+import com.example.to_dolistproject.R;
 import com.example.to_dolistproject.adapter.notes_adapter.AdapterNotes;
+import com.example.to_dolistproject.data.NoteModel;
 import com.example.to_dolistproject.databinding.FragmentHomeBinding;
 import com.example.to_dolistproject.interfaces.OnItemClickListener;
 
-public class HomeFragment extends Fragment {
-    OnItemClickListener onItemClickListener;
-    private FragmentHomeBinding binding;
+public class HomeFragment extends Fragment implements OnItemClickListener {
 
+    AdapterNotes adapterNotes;
+    private FragmentHomeBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,40 +43,13 @@ public class HomeFragment extends Fragment {
 
     private void initAdapter() {
         App.getApp().getDb().noteDao().getAllNotes().observe(getViewLifecycleOwner(), taskList -> {
-            AdapterNotes adapterNotes = new AdapterNotes(taskList);
+            adapterNotes = new AdapterNotes(taskList);
             binding.recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
             binding.recyclerview.setAdapter(adapterNotes);
 
         });
     }
 
-
-//    private void deleteFromDatabase() {
-//        adapterNotes.setOnItemLongClickListener(new OnItemLongClickListener() {
-//            @Override
-//            public void onItemPress(NoteModel noteModel) {
-//                AlertDialog alertDialog = new AlertDialog.Builder(getContext())
-//                        .setTitle("Delete")
-//                        .setMessage("Are you sure you want to delete this task?")
-//                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialogInterface, int i) {
-//                                App.getApp().getDb().noteDao().delete(noteModel);
-//                                Toast.makeText(getActivity(), "You have successfully deleted this task!", Toast.LENGTH_SHORT).show();
-//
-//                            }
-//                        })
-//                        .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialogInterface, int i) {
-//
-//                            }
-//                        })
-//                        .show();
-//
-//            }
-//        });
-//    }
 
     private void initListeners() {
         binding.btnCreate.setOnClickListener(new View.OnClickListener() {
@@ -81,6 +59,31 @@ public class HomeFragment extends Fragment {
                 createTaskFragment.show(requireActivity().getSupportFragmentManager(), "");
             }
         });
+
+
+    }
+
+    @Override
+    public void onItemPress(NoteModel model) {
+        AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                .setTitle("Delete")
+                .setMessage("Are you sure you want to delete this task?")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        App.getApp().getDb().noteDao().delete(model);
+                        Toast.makeText(getActivity(), "You have successfully deleted this task!", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setIcon(getResources().getDrawable(R.drawable.warning))
+                .show();
 
     }
 
